571b189f4f97ffcdfa45c6277fd8293dbdd22e4a
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-21 21:34:51 +0530

    Create RoomController.java

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/controllers/RoomController.java
@@ -0,0 +1,49 @@
+package me.mtron.hostelmanagementsystembackend.controllers;
+
+import me.mtron.hostelmanagementsystembackend.httpentities.Hostel;
+import me.mtron.hostelmanagementsystembackend.httpentities.Room;
+import me.mtron.hostelmanagementsystembackend.repo.RoomRepo;
+import me.mtron.hostelmanagementsystembackend.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api")
+public class RoomController {
+    @Autowired
+    private RoomRepo roomRepo;
+    @Autowired
+    private RoomService roomService;
+
+    @PostMapping("/room")
+    public ResponseEntity<Room> saveRoom(@RequestBody Room r) {
+        r = this.roomService.handleRegistration(r);
+        return ResponseEntity.ok().body(r);
+    }
+
+    @GetMapping("/room")
+    public ResponseEntity<List<Room>> getAllRooms() {
+        List<Room> all = roomRepo.findAll();
+        return ResponseEntity.ok().body(all);
+    }
+
+    @GetMapping("/room/{id}")
+    public ResponseEntity<Optional<Room>> findRoom(@PathVariable Long id) {
+        Optional<Room> h = this.roomRepo.findById(id);
+        return ResponseEntity.ok().body(h);
+    }
+
+    @DeleteMapping("/room/{id}")
+    public ResponseEntity<Optional<Room>> deleteRoom(@PathVariable Long id) {
+        if (roomRepo.existsById(id)) {
+            this.roomRepo.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(400).build();
+    }
+}
