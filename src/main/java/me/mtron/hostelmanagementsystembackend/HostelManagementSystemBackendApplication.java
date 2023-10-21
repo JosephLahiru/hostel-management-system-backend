3693bfc25e0c636f4f4a13224b53b5977224609b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-21 18:58:46 +0530

    init

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/HostelManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package me.mtron.hostelmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mtron/hostelmanagementsystembackend/HostelManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mtron.hostelmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
