910ceadc168dd1a2759ab620f8008163ee17749c
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-24 21:29:41 +0530

    Update RoomRepo.java

--- src/main/java/me/mtron/hostelmanagementsystembackend/repo/RoomRepo.java
+++ src/main/java/me/mtron/hostelmanagementsystembackend/repo/RoomRepo.java
@@ -3,5 +3,4 @@ package me.mtron.hostelmanagementsystembackend.repo;
 import me.mtron.hostelmanagementsystembackend.httpentities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepo extends JpaRepository<Room, Long> {
-}
+public interface RoomRepo extends JpaRepository<Room, Long> {}
