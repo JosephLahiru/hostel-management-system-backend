20c5f0e332a59b630c54b9c46c5af045b3057b47
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-29 00:19:34 +0530

    Create EmployeeController.java

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/controllers/EmployeeController.java
@@ -0,0 +1,30 @@
+package me.mtron.hostelmanagementsystembackend.controllers;
+
+import me.mtron.hostelmanagementsystembackend.DTO.EmployeeDTO;
+import me.mtron.hostelmanagementsystembackend.DTO.LoginDTO;
+import me.mtron.hostelmanagementsystembackend.payload.response.LoginMessage;
+import me.mtron.hostelmanagementsystembackend.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/save")
+    public String saveEmployee(@RequestBody EmployeeDTO employeeDTO){
+        String id = employeeService.addEmployee(employeeDTO);
+        return id;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginEmployee(@RequestBody LoginDTO loginDTO){
+        LoginMessage loginMessage = employeeService.loginEmployee(loginDTO);
+        return ResponseEntity.ok(loginMessage);
+    }
+}
