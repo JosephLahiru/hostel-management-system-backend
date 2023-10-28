b02a0f0dd084a12c9a9162c8c9c69ee6e9882af2
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-29 00:20:03 +0530

    Create EmployeeService.java

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package me.mtron.hostelmanagementsystembackend.service;
+
+import me.mtron.hostelmanagementsystembackend.DTO.EmployeeDTO;
+import me.mtron.hostelmanagementsystembackend.DTO.LoginDTO;
+import me.mtron.hostelmanagementsystembackend.payload.response.LoginMessage;
+
+public interface EmployeeService {
+    String addEmployee(EmployeeDTO employeeDTO);
+
+    LoginMessage loginEmployee(LoginDTO loginDTO);
+}
