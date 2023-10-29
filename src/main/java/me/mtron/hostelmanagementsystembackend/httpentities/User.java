5f71586ba5d2ced18c69e723f4a59e91497b234f
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-29 11:42:14 +0530

    Create User.java

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/httpentities/User.java
@@ -0,0 +1,73 @@
+package me.mtron.hostelmanagementsystembackend.httpentities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "user_id", length = 45)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_name", length = 255)
+    private String userName;
+
+    @Column(name = "email", length = 255)
+    private String email;
+
+    @Column(name = "password", length = 255)
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
