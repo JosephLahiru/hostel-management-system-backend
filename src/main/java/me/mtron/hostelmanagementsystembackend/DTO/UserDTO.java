7e23c8bb7d840a732afc04899f512c3b402378d9
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-29 11:42:09 +0530

    Create UserDTO.java

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/DTO/UserDTO.java
@@ -0,0 +1,60 @@
+package me.mtron.hostelmanagementsystembackend.DTO;
+
+public class UserDTO {
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
