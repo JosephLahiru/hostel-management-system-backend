a229b92157b8d256aec4771809625f039c279aed
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-30 17:56:41 +0530

    Update UserRepo.java

--- src/main/java/me/mtron/hostelmanagementsystembackend/repo/UserRepo.java
+++ src/main/java/me/mtron/hostelmanagementsystembackend/repo/UserRepo.java
@@ -1,15 +1,14 @@
 package me.mtron.hostelmanagementsystembackend.repo;
 
 import me.mtron.hostelmanagementsystembackend.httpentities.User;
+import me.mtron.hostelmanagementsystembackend.models.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@EnableJpaRepositories
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
-    Optional<User> findOneByEmailAndPassword(String email, String password);
-    User findByEmail(String email);
+public interface UserRepo extends JpaRepository<ApplicationUser, Integer> {
+    Optional<ApplicationUser> findByUsername(String username);
 }
