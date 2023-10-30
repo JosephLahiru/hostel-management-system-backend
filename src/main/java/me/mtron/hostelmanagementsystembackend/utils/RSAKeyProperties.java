96bf5d38ffe937f3cadb00b9a995d328b263b2cf
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-30 17:57:33 +0530

    Create RSAKeyProperties.java

---
+++ src/main/java/me/mtron/hostelmanagementsystembackend/utils/RSAKeyProperties.java
@@ -0,0 +1,36 @@
+package me.mtron.hostelmanagementsystembackend.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
+public class RSAKeyProperties {
+
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    public RSAKeyProperties() {
+        KeyPair pair = KeyGeneratorUtility.generateRsaKey();
+        this.publicKey = (RSAPublicKey) pair.getPublic();
+        this.privateKey = (RSAPrivateKey) pair.getPrivate();
+    }
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+}
