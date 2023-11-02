5a48022530c6ef5370c60d3d0cd2aed801449ed7
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-11-02 21:55:35 +0530

    Update ComplaintController.java

--- src/main/java/me/mtron/hostelmanagementsystembackend/controllers/ComplaintController.java
+++ src/main/java/me/mtron/hostelmanagementsystembackend/controllers/ComplaintController.java
@@ -1,6 +1,10 @@
 package me.mtron.hostelmanagementsystembackend.controllers;
 
 import me.mtron.hostelmanagementsystembackend.httpentities.Complaint;
+import me.mtron.hostelmanagementsystembackend.models.DailyReport;
+import me.mtron.hostelmanagementsystembackend.models.StudentRoom;
+import me.mtron.hostelmanagementsystembackend.projection.DailyReportProjection;
+import me.mtron.hostelmanagementsystembackend.projection.StudentRoomProjection;
 import me.mtron.hostelmanagementsystembackend.repo.ComplaintRepo;
 import me.mtron.hostelmanagementsystembackend.service.ComplaintService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,5 +51,27 @@ public class ComplaintController {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.status(400).build();
+    }
+
+    @GetMapping("/daily_report")
+    public ResponseEntity<List<DailyReport>> getDailyReport() {
+        List<DailyReportProjection> projections = complaintRepo.getDailyReport();
+        List<DailyReport> reports = new ArrayList<>();
+        for(DailyReportProjection projection : projections) {
+            DailyReport report = new DailyReport();
+            report.setComplaint_id(projection.getComplaint_id());
+            report.setDescription(projection.getDescription());
+            report.setImage_url(projection.getImage_url());
+            report.setItem_code(projection.getItem_code());
+            report.setItem_name(projection.getItem_name());
+            report.setRoom_no(projection.getRoom_no());
+            report.setHostel_type(projection.getHostel_type());
+            report.setStu_no(projection.getStu_no());
+            report.setStudent_name(projection.getStudent_name());
+            report.setStatus(projection.getStatus());
+            report.setComplaint_created_date(projection.getComplaint_created_date());
+            reports.add(report);
+        }
+        return ResponseEntity.ok(reports);
     }
 }
