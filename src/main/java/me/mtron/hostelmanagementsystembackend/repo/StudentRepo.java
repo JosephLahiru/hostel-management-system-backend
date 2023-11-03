80261110bd8daa195b9a47afcaff3f6641b36fbf
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-11-03 18:33:41 +0530

    Update StudentRepo.java

--- src/main/java/me/mtron/hostelmanagementsystembackend/repo/StudentRepo.java
+++ src/main/java/me/mtron/hostelmanagementsystembackend/repo/StudentRepo.java
@@ -12,7 +12,7 @@ public interface StudentRepo extends JpaRepository<Student, Long> {
             "FROM room_property rp " +
             "JOIN room r ON rp.room_id = r.id " +
             "JOIN property p ON rp.property_id = p.id " +
-            "JOIN student s ON r.room_no = s.room_number" +
+            "JOIN student s ON r.room_no = s.room_number " +
             "WHERE rp.property_id = :propertyId", nativeQuery = true)
     List<StudentRoomProjection> findStudentRoomByPropertyId(String propertyId);
 
