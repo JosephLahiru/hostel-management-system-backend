bfafe402dbb57bf3ddfcef2182508d4eb019c3d2
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-11-05 20:01:54 +0530

    Update ComplaintRepo.java

--- src/main/java/me/mtron/hostelmanagementsystembackend/repo/ComplaintRepo.java
+++ src/main/java/me/mtron/hostelmanagementsystembackend/repo/ComplaintRepo.java
@@ -2,6 +2,8 @@ package me.mtron.hostelmanagementsystembackend.repo;
 
 import me.mtron.hostelmanagementsystembackend.httpentities.Complaint;
 import me.mtron.hostelmanagementsystembackend.projection.DailyReportProjection;
+import me.mtron.hostelmanagementsystembackend.projection.TopComplainedItemsProjection;
+import me.mtron.hostelmanagementsystembackend.projection.TopComplainedRoomsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +15,10 @@ public interface ComplaintRepo extends JpaRepository<Complaint, Long> {
 
     @Query(value = "SELECT * FROM monthly_report_view;", nativeQuery = true)
     List<DailyReportProjection> getMonthlyReport();
+
+    @Query(value = "SELECT * FROM top_complained_rooms;", nativeQuery = true)
+    List<TopComplainedRoomsProjection> getRoomComplaints();
+
+    @Query(value = "SELECT * FROM top_complained_item;", nativeQuery = true)
+    List<TopComplainedItemsProjection> getItemComplaints();
 }
